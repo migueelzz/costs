17652af41914e3f23b45c4b6cf4160903f448f8f
migueelzz <miguellemes005@gmail.com>
2022-07-15 13:51:22 -0300

    :sparkles: Introduce new features
    styling project frontend

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import LinkButton from '../layout/LinkButton';
 function Home() {
   return (
     <section className={styles.homeContainer}>
-      <h1>Welcome to <span>Costs</span></h1>
+      <h1>Welcome to<span> Costs</span></h1>
       <p>Start managing your projects right now!</p>
       <LinkButton to="/newproject" text="Create Project" />
       <img src={saving} alt="Costs" />
